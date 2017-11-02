5f142d8d63e663d5601205d0f0c0cbcd5fbdd8cf
Ulrich Stärk <ulrich_staerk@mckinsey.com>
2017-11-02 01:14:53 +0100

    basic motor manipulation

--- ev3api.py
+++ ev3api.py
@@ -8,6 +8,7 @@ import rpyc
 from flask import json
 from flask import jsonify
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 conn = rpyc.classic.connect('192.168.0.40')
 ev3 = conn.modules['ev3dev.ev3']
@@ -19,10 +20,31 @@ def list_motors():
     motors = [motor_to_dict(attached_motor) for attached_motor in attached_motors]
     return jsonify(motors)
 
+@app.route('/motors/<string:port>', methods=['PUT'])
+def change_motor(port):
+    payload = request.json
+    handle_payload(port, payload)
+    return jsonify({})
+
+def handle_payload(port, payload):
+    motor = ev3.Motor(port); assert motor.connected
+    if not payload['state']:
+        motor.reset()
+    elif payload['state'] == 'running':
+        if 'running' not in motor.state:
+            motor.run_direct(duty_cycle_sp = 100)
+        handle_attributes(motor, payload)
+    else:
+        raise InvalidUsage('Unsupported state.')
+
+def handle_attributes(motor, payload):
+    assert motor.connected
+    if 'duty_cycle_sp' in payload:
+        motor.duty_cycle_sp = payload['duty_cycle_sp']
+
 def motor_to_dict(motor):
     return dict(address = motor.address,
-                # TODO: find out why this cannot be encoded as JSON
-                # commands = motor.commands,
+                commands = motor.commands,
                 driver_name = motor.driver_name,
                 duty_cycle = motor.duty_cycle,
                 duty_cycle_sp = motor.duty_cycle_sp,
@@ -39,8 +61,29 @@ def motor_to_dict(motor):
                 ramp_up_sp = motor.ramp_up_sp,
                 speed = motor.speed,
                 speed_sp = motor.speed_sp,
-                # state = motor.state,
+                state = motor.state,
                 stop_action = motor.stop_action,
-                # stop_actions = motor.stop_actions,
+                stop_actions = motor.stop_actions,
                 time_sp = motor.time_sp
                 )
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
